2129826c30ddc313d29bf7b35493db6466429a78
Joshvu93 <joshvu93@gmail.com>
2019-11-01 15:04:45 -0700

    changed calculate function to let

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,50 +1,46 @@
+/* eslint-disable max-len */
+/* eslint-disable no-mixed-operators */
 import React from 'react';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      balance: "",
-      rate: "",
-      term: "",
-      output: "",
+      balance: 0,
+      rate: 0,
+      term: 15,
     };
-
-    this.calcualte = this.calcualte.bind(this);
+    this.changeInput = this.changeInput.bind(this);
+    this.calculate = this.calculate.bind(this);
   }
 
-  calcualte() {
-    const p = Numbers(this.state.balance);
-    const r = Numbers(this.state.rate) / 100 / 12;
-    const n = Numbers(this.state.terms) * 12;
-    const monthlyPayment = (p * (r * math.pow((1 + r), n) / (Math.pow(1 + r, n) - 1))).toFixed(2);
-    this.setState({ submit: monthlyPayment })
+  changeInput(object) {
+    this.setState({ [object.target.name]: [object.target.value] });
+  }
+
+  calculate() {
+    let rate = (this.state.rate) / 100 / 12;
+    let term = (this.state.term) * 12;
+    let balance = (this.state.balance);
+    let monthlyPayment = (balance * (rate * Math.pow((1 + rate), term) / (Math.pow(1 + rate, term) - 1))).toFixed(2);
+    this.setState({ submit: monthlyPayment });
   }
 
   render() {
     return (
-      <form className='for-horizontal'>
-        <div className='container'>
-          <h1>Mortgage Calculator</h1>
-          <div>
-            <label>Loan Balance</label>
-            <input className="balance" type='number' placeholder='0'></input>
-          </div>
-          <div>
-            <label>Interest Rate (%) </label>
-            <input className="rate" type='number' step='0.01' placeholder='0'></input>
-          </div>
-          <div>
-            <label>Loan Term (years) </label>
-            <select className="term">
-              <option value='15'>15</option>
-              <option value='30'>30</option>
-            </select>
-          </div>
-          <button className="submit">Submit</button>
-          <div id='output' className='output'></div>
+      <div className='container'>
+        <div name='input' id='input'>
+          <h3>Mortgage Calculator</h3>
+          <input name='balance' type='number' value={this.state.balance} onChange={this.changeInput} />
+          <input name='rate' type='number' step='0.01' value={this.state.rate} onChange={this.changeInput} />
+          <select name='term' value={this.state.term} onChange={this.changeInput}>
+            <option value='15'>15</option>
+            <option value='30'>30</option>
+          </select>
+          <button name='submit' onClick={this.calculate}>submit</button>
         </div>
-      </form>
+        <div id='output' name='output'> ${this.state.submit} is your payment. </div>
+      </div>
     );
   }
 }
